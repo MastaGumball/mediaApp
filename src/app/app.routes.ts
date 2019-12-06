72f433ded881b87052883db5e7649044a41e31b3
Ibrahim El Yahyaoui <ely.ibrahim@gmail.com>
2019-12-06 02:30:45 +0100

    responsive grid on addMovieComponent + imageNotFound

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,33 @@
 import {MovieslistComponent} from './movieslist/movieslist.component';
 import {MovieDetailsComponent} from './movie-details/movie-details.component';
-import {ActorOverviewComponent} from './actor-overview/actor-overview.component'
+import {ActorOverviewComponent} from './actor-overview/actor-overview.component';
 import {Routes} from '@angular/router';
 import { ActorDetailsComponent } from './actor-details/actor-details.component';
 import { MoviesAddComponent } from './movies-add/movies-add.component';
 
 export const MovieAppRoutes: Routes = [
+  {
+    path: '',
+    component: MovieslistComponent
+  },
     {
-    path:'movies', 
+    path: 'movies',
     component: MovieslistComponent
     },
     {
-        path:'movies/add',
+        path: 'movies/add',
         component: MoviesAddComponent
     },
     {
-        path:'movies/:id',
+        path: 'movies/:id',
         component: MovieDetailsComponent
     },
     {
-        path:'actors',
+        path: 'actors',
         component: ActorOverviewComponent,
         children: [
             {
-                path:':id',
+                path: ':id',
                 component: ActorDetailsComponent
             }]
     },
